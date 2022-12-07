a2be7b27dba8552e401c4289b3c82023335de426
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2022-12-07 14:27:12 +0100

    :fire: :art: Mobile Navbar :sparkles: :globe_with_meridians:

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 import './style.css'
+
+const mobileDarkLayer = document.querySelector('#mobileDarkLayer');
+const mobileNavbar = document.querySelector('#mobileNavbar');
+const mobileBurgerMenu = document.querySelector('#mobileBurgerMenu');
+
+mobileBurgerMenu.addEventListener('click', ()=>{
+    mobileDarkLayer.classList.remove('hidden')
+    mobileNavbar.classList.remove('hidden')
+});
+
+mobileDarkLayer.addEventListener('click', ()=>{
+    mobileDarkLayer.classList.add('hidden')
+    mobileNavbar.classList.add('hidden')
+});
