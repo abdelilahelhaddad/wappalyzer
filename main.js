0a11110220b6207e6938b1f6037a643d872ad2ac
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2022-12-10 16:02:33 +0100

    :fire: :art: Mobile Navbar :sparkles: :globe_with_meridians:

--- main.js
+++ main.js
@@ -4,26 +4,41 @@ const mobileDarkLayer = document.querySelector('#mobileDarkLayer');
 const mobileNavbar = document.querySelector('#mobileNavbar');
 const mobileBurgerMenu = document.querySelector('#mobileBurgerMenu');
 
-mobileBurgerMenu.addEventListener('click', ()=>{
-    mobileDarkLayer.classList.remove('hidden')
-    mobileNavbar.classList.remove('hidden')
+mobileBurgerMenu.addEventListener('click', () => {
+    mobileDarkLayer.classList.remove('hidden');
+    mobileNavbar.classList.remove('hidden');
 });
 
-mobileDarkLayer.addEventListener('click', ()=>{
-    mobileDarkLayer.classList.add('hidden')
-    mobileNavbar.classList.add('hidden')
+mobileDarkLayer.addEventListener('click', () => {
+    mobileDarkLayer.classList.add('hidden');
+    mobileNavbar.classList.add('hidden');
 });
+
+//Toggle Products Menu
 
 const productsNavBar = document.querySelector('#productsNavBar');
 const productsDropDown = document.querySelector('#productsDropDown');
 
-productsNavBar.addEventListener('click', ()=>{
+productsNavBar.addEventListener('click', () => {
     productsDropDown.classList.toggle('hidden');
 });
+
+//Toggle Recources Menu
 
 const resourcesNavBar = document.querySelector('#resourcesNavBar');
 const resourcesDropDown = document.querySelector('#resourcesDropDown');
 
-resourcesNavBar.addEventListener('click', ()=>{
+resourcesNavBar.addEventListener('click', () => {
     resourcesDropDown.classList.toggle('hidden');
+});
+
+//Close menu when click outside
+
+window.addEventListener('click', function (e) {
+    if (e.target != productsNavBar) {
+        productsDropDown.classList.add('hidden');
+    }
+    if (e.target != resourcesNavBar) {
+        resourcesDropDown.classList.add('hidden');
+    }
 });
